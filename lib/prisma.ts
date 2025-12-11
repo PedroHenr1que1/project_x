83686227d24f4f4f3e1ee5596850a99d0301a54c
PedroHenr1que1 <pedrohenriquefaria9@gmail.com>
2025-12-11 20:01:53 -0300

    revert: :bug: Reverte para Adapter Neon para corrigir erro de tipagem

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import { Client as NeonClient } from '@neondatabase/serverless'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
@@ -10,7 +12,11 @@ function createPrismaClient() {
   if (!connectionString) {
     throw new Error('DATABASE_URL is not defined in the environment.')
   }
-  return new PrismaClient()
+
+  const neon = new NeonClient({ connectionString })
+  const adapter = new PrismaNeon(neon)
+  
+  return new PrismaClient({ adapter })
 }
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient()
