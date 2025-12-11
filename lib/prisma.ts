79871d48d69d489be208d01566543121761e2420
PedroHenr1que1 <pedrohenriquefaria9@gmail.com>
2025-12-11 19:58:14 -0300

    fix: :fire: Removendo o Adapter Neon

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,4 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaNeon } from '@prisma/adapter-neon'
-import { Client as NeonClient } from '@neondatabase/serverless'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
@@ -10,14 +8,9 @@ function createPrismaClient() {
   const connectionString = process.env.DATABASE_URL
   
   if (!connectionString) {
-    throw new Error('DATABASE_URL is not defined')
+    throw new Error('DATABASE_URL is not defined in the environment.')
   }
-
-  const neon = new NeonClient({ connectionString })
-  
-  const adapter = new PrismaNeon(neon)
-  
-  return new PrismaClient({ adapter })
+  return new PrismaClient()
 }
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient()
